7793a54bebbfd6d862c38b9f9722b6f7b1158c9a
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-08-30 11:23:31 -0300

    Sending message trough the commandLine in the application

--- src/main/java/com/example/kafka_template/KafkaTemplateApplication.java
+++ src/main/java/com/example/kafka_template/KafkaTemplateApplication.java
@@ -1,7 +1,10 @@
 package com.example.kafka_template;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaTemplateApplication {
@@ -10,4 +13,10 @@ public class KafkaTemplateApplication {
 		SpringApplication.run(KafkaTemplateApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("template", "Hello Kafka");
+		};
+	}
 }
