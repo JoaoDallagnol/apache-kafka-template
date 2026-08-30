2335b092eb10cee1f7532491136cae9e6bdc690d
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-08-30 16:57:08 -0300

    Finishin template project for kafka

---
+++ src/main/java/com/example/kafka_template/MessageController.java
@@ -0,0 +1,21 @@
+package com.example.kafka_template;
+
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/messages")
+@AllArgsConstructor
+public class MessageController {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest request) {
+        kafkaTemplate.send("template", request.message());
+    }
+}
---
+++ src/main/java/com/example/kafka_template/MessageRequest.java
@@ -0,0 +1,5 @@
+package com.example.kafka_template;
+
+public record MessageRequest(
+        String message
+) {}
